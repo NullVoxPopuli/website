ea2ec5e1a6a163e76fca749164c0c1ceecce7283
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-08-28 10:02:39 -0400

    re-add IE-11 for... reasons

--- config/targets.js
+++ config/targets.js
@@ -7,6 +7,13 @@ const browsers = [
   'last 1 Edge versions'
 ];
 
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('ie 11');
+}
+
 module.exports = {
   browsers,
   node: 'current',
